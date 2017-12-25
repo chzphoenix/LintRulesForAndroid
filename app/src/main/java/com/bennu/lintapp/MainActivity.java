97cd1fbfe8c7ee6ff56fe234b5eb20bbce32dbcd
chz <cuihz@knowbox.cn>
2017-12-25 18:07:05 +0800

    modify mainactivity

--- app/src/main/java/com/bennu/lintapp/MainActivity.java
+++ app/src/main/java/com/bennu/lintapp/MainActivity.java
@@ -4,29 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends BaseActivity {
-    private A a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a = new A();
-        B b = new B();
-        Log.e("ssss", b.count + "");
-        Log.e("sss", a.i + "");
+        Log.e("test", "use log.e");
         init();
     }
 
     @Override
     void init() {
 
-    }
-
-    class A{
-        private int i = 2;
-    }
-
-    class B{
-        private int count = a.i;
     }
 }
